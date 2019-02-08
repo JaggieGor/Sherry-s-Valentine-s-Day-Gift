cfadf8aa72502b7371df47a67e379d47be23f534
jaggie115 <619613156@qq.com>
2019-02-08 22:39:53 +0800

    Initial commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/comehope/pen/xJvERW.
+
+ Watch interactive video on Scrimba: https://scrimba.com/p/pEgDAM/cm94eu6
---
+++ js/index.js
@@ -0,0 +1,31 @@
+// const words = [
+//     '愛', 'Love', '冼', '美', '茵',
+//     'Amor', 'Любовь', 'الحب', 'प्यार', 'Cinta',
+//     'Αγάπη', '사랑', 'Liefde', 'Dashuri', 'Каханне',
+//     'Ljubav', 'Láska', 'Armastus', 'Mahal', 'אהבה', 
+//     'Szerelem', 'Grá', 'Mīlestība', 'Meilė', 'Любов', 
+//     'Љубовта', 'Cinta', 'عشق', 'Dragoste', 'Láska', 
+//     'Renmen', 'ፍቅር', 'munaña', 'Sevgi', 'Љубав', 
+//     'karout', 'amà', 'amôr', 'kærleiki', 'mborayhu', 
+//     'Upendo', 'sòòyayyàà', 'ljubav', 'Սեր', 'сүю', 
+//     'сүйүү', 'tia', 'aroha', 'KHAIR', 'प्रेम', 
+//     'kjærlighet', 'munay', 'jecel', 'Kärlek', 'soymek', 
+//     'Mahal', 'ярату', 'محبت', 'sopp', 'uthando', 
+//     'ความรัก', 'Aşk', 'Tình yêu', 'ליבע'];
+
+const words = "Jaggie love three things in this world . Sun , Moon and You . Sun for morning , Moon for night and Sherry forever .".split(' ');
+const dom = {
+    love: document.querySelector('.love')
+}
+
+dom.love.style.setProperty('--particles', words.length)
+
+words.forEach((word, i) => {
+    let span = document.createElement('span')
+    span.style.setProperty('--n', i + 1)
+    if (word=='Sherry'||word=='Jaggie'){
+        span.classList.add("highlight")
+    }
+    span.innerText = word
+    dom.love.appendChild(span)
+})
